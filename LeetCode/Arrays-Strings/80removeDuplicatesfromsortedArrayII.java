853d1b7614cc6069467499a09291fd9e1c20539c
Suraj Senapati <surajsenapati58@gmail.com>
2024-10-17 09:06:10 +0530

    comments

--- LeetCode/Arrays-Strings/80removeDuplicatesfromsortedArrayII.java
+++ LeetCode/Arrays-Strings/80removeDuplicatesfromsortedArrayII.java
@@ -6,6 +6,7 @@ class Solution {
         //Two Pointers
         int l=1;
         int count=1;
+        //We will count the number of duplicates
         for(int r=1;r<nums.length;r++){
             if(nums[r]==nums[r-1]){
                 count++;
@@ -13,6 +14,7 @@ class Solution {
             else{
                 count=1;
             }
+            //if any duplicates atmost two
             if(count<=2){
                 nums[l]=nums[r];
                 l++;
