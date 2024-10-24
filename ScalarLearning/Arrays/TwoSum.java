f9168747eb609abdc4517ddb5b596e35fb0ad3a2
Suraj Senapati <surajsenapati58@gmail.com>
2024-10-24 18:47:56 +0530

    Update

---
+++ ScalarLearning/Arrays/TwoSum.java
@@ -0,0 +1,22 @@
+package ScalarLearning.Arrays;
+import java.sql.SQLOutput;
+import java.util.*;
+public class TwoSum {
+    public static int [] twoSum(int[] array,int target){
+        HashMap<Integer, Integer> map= new HashMap<>();
+        for(int i=0;i<array.length;i++){
+            int second=target-array[i];
+            if(map.containsKey(second)){
+                return new int[] {map.get(second),i};
+            }
+            map.put(array[i], i);
+        }
+        throw new IllegalArgumentException("No Two Sum Present.");
+    }
+    public static void main(String[] args) {
+        int[] arr={11, 3, 7, 9, 14, 2};
+        int target=17;
+        int ans[]=twoSum(arr, target);
+        System.out.println(Arrays.toString(ans));
+    }
+}
