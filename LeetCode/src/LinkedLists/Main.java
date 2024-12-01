00a7b62b78b51b0f64fb1b394842b0c0450cc724
Suraj Senapati <surajsenapati58@gmail.com>
2024-12-01 23:05:58 +0530

    Update

---
+++ LeetCode/src/LinkedLists/Main.java
@@ -0,0 +1,14 @@
+package LinkedLists;
+
+public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList list=new SinglyLinkedList();
+        list.insertFirst(1);
+        list.insertFirst(2);
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list.insertFirst(5);
+        list.display();
+
+    }
+}
--- LeetCode/src/LinkedLists/SinglyLinkedList.java
+++ LeetCode/src/LinkedLists/SinglyLinkedList.java
@@ -18,4 +18,21 @@ public class SinglyLinkedList {
             this.next=null;
         }
     }
+    public void insertFirst(int val){
+        Node node=new Node(val);
+        node.next=head;
+        head=node;
+        if (tail==null){
+            tail=head;
+        }
+        size+=1;
+    }
+    public void display(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.value+" -> ");
+            temp=temp.next;
+        }
+        System.out.println("END");
+    }
 }
