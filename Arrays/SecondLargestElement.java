46ded4b1d995be99a762ace5ba7fd8080fa2d20c
Suraj Senapati <surajsenapati58@gmail.com>
2025-01-26 18:35:38 +0530

    Arrays updated

--- Arrays/SecondLargestElement.java
+++ Arrays/SecondLargestElement.java
@@ -1,8 +1,19 @@
 package Arrays;
-
 import java.util.Arrays;
 
 public class SecondLargestElement {
+    static int LargestElement(int arr[]){
+        if(arr.length==0){
+            return -1;
+        }
+        int largest=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>largest){
+                largest=arr[i];
+            }
+        }
+        return largest;
+    }
     static int brute(int arr[]){
         if(arr.length==0){
             return 0;
@@ -17,10 +28,37 @@ public class SecondLargestElement {
             }
         }
         return sec_largest;
-
+        //T.C= O(nlogn+n)
+    }
+    static int better(int arr[]){
+        int largest=LargestElement(arr);
+        int second_largest=-1;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>second_largest && arr[i]!=largest){
+                second_largest=arr[i];
+            }
+        }
+        return second_largest;
+        //T.C=O(n+n)=O(2n) approximately =O(n)
+    }
+    static int optimal(int arr[]){
+        int largest=arr[0];
+        int sec_largest=-1;
+        for(int i=1;i<arr.length-1;i++){
+            if(arr[i]>largest){
+                sec_largest=largest;
+                largest=arr[i];
+            }
+            else if(arr[i]<largest && arr[i]>sec_largest){
+                sec_largest=arr[i];
+            }
+        }
+        return sec_largest;
     }
     public static void main(String[] args) {
         int arr[]={1,2,4,7,7,5};
         System.out.println(brute(arr));
+        System.out.println(better(arr));
+        System.out.println(optimal(arr));
     }
 }
---
+++ Arrays/SecondSmallestElement.java
@@ -0,0 +1,21 @@
+package Arrays;
+public class SecondSmallestElement {
+    static int optimal(int arr[]){
+        int smallest=arr[0];
+        int ssmallest=Integer.MAX_VALUE;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]<smallest){
+                ssmallest=smallest;
+                smallest=arr[i];
+            }
+            else if(arr[i]!=smallest && arr[i]<ssmallest){
+                ssmallest=arr[i];
+            }
+        }
+        return ssmallest;
+    }
+    public static void main(String[] args) {
+        int arr[]={7,2,4,5,1};
+        System.out.println(optimal(arr));
+    }
+}
