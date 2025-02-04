9da72408fd0a44cc2a867b22074359e810371cc4
Suraj Senapati <surajsenapati58@gmail.com>
2025-02-04 20:29:08 +0530

    Better Appraoch

--- Arrays/FindTheMissingNumber.java
+++ Arrays/FindTheMissingNumber.java
@@ -17,10 +17,26 @@ public class FindTheMissingNumber {
             }
         }
         return ans;
+        //T.C=O(n*n)(nearly)
     }
+    public static int better(int arr[], int n){
+        HashSet<Integer> hash=new HashSet<>();
+        for(int i=0;i<n-1;i++){
+            hash.add(arr[i]);
+        }
+        for(int i=1;i<=n;i++){
+            if(!hash.contains(i)){
+                return i;
+            }
+        }
+        return n+1;
+        //T.C=O(m+n)
+        //S.C=0(n-1)
+    }
+    
     public static void main(String[] args) {
         int arr[]={1,2,4,5};
         int n=5;
-        System.out.println("The missing number in the array is: "+brute(arr, n));
+        System.out.println("The missing number in the array is: "+better(arr, n));
     }
 }
