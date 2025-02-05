6943ddaa025eb5ad9b833612ade4609489ea2e4b
Suraj Senapati <surajsenapati58@gmail.com>
2025-02-05 21:38:00 +0530

    update

--- Arrays/FindTheMissingNumber.java
+++ Arrays/FindTheMissingNumber.java
@@ -47,20 +47,19 @@ public class FindTheMissingNumber {
     //XOR approach
     public static int optimal_2(int arr[],int n){
         int xor1=0;
-        for(int i=1;i<=n;i++){
-            xor1^=i;
-        }
         int xor2=0;
         for(int i=0;i<n-1;i++){
             xor2^=arr[i];
+            xor1^=i+1;
         }
+        xor1^=n;
         return xor1^xor2;
-        //T.C=O(n+m)
+        //T.C=O(n)
         //S.C=O(1)
     }
     public static void main(String[] args) {
-        int arr[]={1,2,4,5};
-        int n=5;
+        int arr[]={2,19,10,1,5,3,13,18,24,23,9,22,28,25,6,27,4,20,21,26,11,8,17,15,16,12,7,14};
+        int n=29;
         System.out.println("The missing number in the array is: "+optimal_2(arr, n));
     }
 }
