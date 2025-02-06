0623a46fbf5194f2f9bbf6540058cbe43c9e058e
Suraj Senapati <surajsenapati58@gmail.com>
2025-02-06 23:17:45 +0530

    brute

---
+++ Arrays/LongestSubarrayWithGivenSum.java
@@ -0,0 +1,25 @@
+package Arrays;
+import java.util.*;
+public class LongestSubarrayWithGivenSum {
+    public static int brute(int arr[],long k){
+        int n=arr.length;
+        int len=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int sum=0;
+                for(int a=i;a<=j;a++){
+                    sum+=arr[a]; 
+                }
+                if(sum==k){
+                    len=Math.max(len, j-i+1);
+                }
+            }
+        }
+        return len;
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,1,1,1,2,4,2,1};
+        int k=3;
+        System.out.println(brute(arr, k));
+    }
+}
