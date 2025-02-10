024d360c19945d68cd7e267ef8ac1749cdffe7f2
Suraj Senapati <surajsenapati58@gmail.com>
2025-02-10 23:15:32 +0530

    Optimized brute

--- Arrays/LongestSubarrayWithGivenSum.java
+++ Arrays/LongestSubarrayWithGivenSum.java
@@ -17,9 +17,20 @@ public class LongestSubarrayWithGivenSum {
         }
         return len;
     }
+    public static int optimizedBrute(int[] arr,int k){
+        int len=0;
+        for(int i=0;i<arr.length;i++){
+            int sum=0;
+            for(int j=i;j<arr.length;j++){
+                sum+=arr[j];
+                if(sum==k) len=Math.max(len, j-i+1);
+            }
+        }
+        return len;
+    }
     public static void main(String[] args) {
         int arr[]={1,2,3,1,1,1,2,4,2,1};
         int k=3;
-        System.out.println(brute(arr, k));
+        System.out.println(optimizedBrute(arr, k));
     }
 }
