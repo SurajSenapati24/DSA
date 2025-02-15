1fd50b6aea11a6f833787900ad915f62d407da61
Suraj Senapati <surajsenapati58@gmail.com>
2025-02-15 21:13:52 +0530

    Selection Sort

---
+++ SortingTechniques/SelectionSort.java
@@ -0,0 +1,23 @@
+package SortingTechniques;
+import java.util.*;
+public class SelectionSort {
+    static void selectionSort(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            int minidx=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[minidx]){
+                    minidx=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[minidx];
+            arr[minidx]=temp;
+        }
+        
+    }
+    public static void main(String[] args) {
+        int arr[] = {99, 2, 34, 1, 46, 8, 4};
+        selectionSort(arr);
+        System.out.println("After sorting: " + Arrays.toString(arr));
+    }    
+}
