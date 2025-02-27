2583804ea1c7893e755341d05ee8d66736c87310
Suraj Senapati <surajsenapati58@gmail.com>
2025-02-27 23:17:42 +0530

    Stock buy and sell python

---
+++ PYTHON/Arrays/StockBuyAndSell.py
@@ -0,0 +1,24 @@
+def brute(prices):
+    profit = 0
+    n = len(prices)
+    for i in range(n - 1):
+        for j in range(i, n):
+            profit = max(profit, prices[j] - prices[i])
+    return profit  # T.C = O(N^2)
+
+def optimal(prices):
+    n = len(prices)
+    min_price = prices[0]
+    profit = 0
+    for i in range(1, n):
+        cur = prices[i]
+        if min_price < cur:
+            profit = max(profit, cur - min_price)
+        else:
+            min_price = cur
+    return profit  # T.C = O(N)
+
+if __name__ == "__main__":
+    prices = [17, 20, 11, 9, 12, 6]
+    print(brute(prices))  # Testing the brute force approach
+    print(optimal(prices))  # Testing the optimal approach
