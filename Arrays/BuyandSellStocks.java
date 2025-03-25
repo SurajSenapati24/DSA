f7d9ae5d1a64ca8d5da70645362afe84044ce06d
Suraj Senapati <surajsenapati58@gmail.com>
2025-03-25 22:26:49 +0530

    Update

---
+++ Arrays/BuyandSellStocks.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class BuyandSellStocks {
+    public static int maxProfit(int prices[]){
+        int profit=0;
+        int minimum=Integer.MAX_VALUE;
+        for(int i=0;i<prices.length;i++){
+            if(minimum>prices[i]){
+                minimum=prices[i];
+            }
+            profit=Math.max(profit,prices[i]-minimum);
+        }
+        return profit;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int prices[]={7,1,5,3,6,4};
+        int profit=maxProfit(prices);
+        System.out.println(profit);
+    }
+}
