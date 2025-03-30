11bb7ea81719d5fd4ea0b7be0ad6e8db45ca2692
Suraj Senapati <surajsenapati58@gmail.com>
2025-03-30 21:08:38 +0530

    Tabulation form of DP

--- DynamicProgramming/FibonacciUsingDP.java
+++ DynamicProgramming/FibonacciUsingDP.java
@@ -1,20 +1,32 @@
 import java.util.*;
 public class FibonacciUsingDP {
+    //Memoization
     public final static int max=100;
-    public static int fact(int n,int dp[]){
+    public static int memoization(int n,int dp[]){
         if(n<=1) return n;
         if(dp[n]!=-1) return dp[n];
         else{
-            dp[n]=fact(n-1, dp)+fact(n-2, dp);
+            dp[n]=memoization(n-1, dp)+memoization(n-2, dp);
         }
         return dp[n];
     }
+    //Tabulation
+    public static int tabulation(int n){
+        int secondprev=0,prev=1;
+        int curr=0;
+        for(int i=2;i<=n;i++){
+            curr=prev+secondprev;
+            secondprev=prev;
+            prev=curr;
+        }
+        return curr;
+    } 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the nth term: ");
         int n=sc.nextInt();
         int dp[]=new int[max];
         Arrays.fill(dp, -1);
-        System.out.println(fact(n, dp));
+        System.out.println(tabulation(n));
     }
 }
