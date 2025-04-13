11324827edce70b356648682fc8b5ae2be79613c
Suraj Senapati <surajsenapati58@gmail.com>
2025-04-13 18:32:16 +0530

    update

---
+++ Arrays/RotateMatrixBy90Degrees.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class RotateMatrixBy90Degrees {
+    public static int[][] brute(int arr[][],int n){
+        int ans[][]=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+
+            }
+        }
+        return ans;
+
+    }
+    public static void main(String[] args) {
+        
+    }
+
+}
