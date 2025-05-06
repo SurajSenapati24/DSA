b10d50a2e552d5c286093a772a9f86db12d9ed2b
Suraj Senapati <surajsenapati58@gmail.com>
2025-05-06 20:52:35 +0530

    Optimal approach along with additonal functions added

--- Arrays/RotateImageby90degrees.java
+++ Arrays/RotateImageby90degrees.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class RotateImageby90degrees {
     public static int[][] brute(int arr[][]){
         int n=arr.length;
@@ -9,6 +10,23 @@ public class RotateImageby90degrees {
         }
         return ans;
     }
+    public static int[][] optimal(int arr[][]){
+        int n=arr.length;
+        transpose(arr);
+        for(int i=0;i<n;i++){
+            reverseRow(arr, i, 0, n-1);
+        }
+        return arr;
+    }
+    public static void reverseRow(int arr[][], int i, int start, int end){
+        while(start<=end){
+            int temp=arr[i][start];
+            arr[i][start]=arr[i][end];
+            arr[i][end]=temp;
+            start++;
+            end--;
+        }
+    }
     public static void printArray(int arr[][]){
         int n=arr.length;
         for(int i=0;i<n;i++){
@@ -18,10 +36,24 @@ public class RotateImageby90degrees {
             System.out.println();
         }
     }
+    public static void transpose(int arr[][]){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }
+    }
     public static void main(String[] args) {
         int arr[][]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
         printArray(arr);
         int ans[][]=brute(arr);
         printArray(ans);
+        System.out.println("Optimal Solution Results: ");
+        optimal(arr);
+        printArray(arr);
+
     }
 }
