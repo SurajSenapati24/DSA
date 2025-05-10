c57852cc1696d4638b15ac2bd5143830e0f4e1de
Suraj Senapati <surajsenapati58@gmail.com>
2025-05-11 00:32:04 +0530

    optimal solution of 3 sum added

--- Arrays/ThreeSum.java
+++ Arrays/ThreeSum.java
@@ -50,8 +50,33 @@ public class ThreeSum {
         return ans;
         //T.C=O(N^2)
     }
+    public static List<List<Integer>> optimal(int nums[]){
+        int n=nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum>0) k--;
+                else if(sum<0) j++;
+                else{
+                    List<Integer> temp=new ArrayList<>(Arrays.asList(nums[i],nums[j],nums[k]));
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                }
+            }
+        }
+        return ans;
+        //T.C=O(nlogn+n^2)
+    }
     public static void main(String[] args) {
         int nums[]={-1,0,1,2,-1,-4};
-        System.out.println(better(nums));
+        System.out.println(optimal(nums));
     }
 }
