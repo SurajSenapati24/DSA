a6112bba6c9f57055aeda132bc435df07b6a1007
Suraj Senapati <surajsenapati58@gmail.com>
2025-06-11 21:27:10 +0530

    Update

--- Arrays/CountNumberOfSubArrayysWithSumK.java
+++ Arrays/CountNumberOfSubArrayysWithSumK.java
@@ -46,8 +46,6 @@ public class CountNumberOfSubArrayysWithSumK {
         return count;
         //T.C=O(N)
     }
-    
-
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3, -3, 1, 1, 1, 4, 2, -3 };
         int k = 3;
