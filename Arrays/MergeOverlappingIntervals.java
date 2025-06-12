40c7ce273e4a845450bf937912496884ca130653
Suraj Senapati <surajsenapati58@gmail.com>
2025-06-12 22:57:32 +0530

    update

---
+++ Arrays/MergeOverlappingIntervals.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class MergeOverlappingIntervals {
+    public static int[][] brute(int arr[][]){
+        int n=arr.length;
+        int ans[][]=new int[n][2];
+        for(int i=0;i<n;i++){
+            
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
