7d25aceb14db93292524f708305f03cb40d3240f
Suraj Senapati <surajsenapati58@gmail.com>
2025-06-14 20:03:07 +0530

    Brute for merge Overlapping arrays

--- Arrays/MergeOverlappingIntervals.java
+++ Arrays/MergeOverlappingIntervals.java
@@ -2,12 +2,30 @@ import java.util.*;
 public class MergeOverlappingIntervals {
     public static int[][] brute(int arr[][]){
         int n=arr.length;
-        int ans[][]=new int[n][2];
+        Arrays.sort(arr,(a,b)-> Integer.compare(a[0], b[0]));
+        List<int []> res=new ArrayList<>();
         for(int i=0;i<n;i++){
-            
+            int start=arr[i][0];
+            int end=arr[i][1];
+            if(!res.isEmpty() && res.get(res.size()-1)[1]>=end){
+                continue;
+            }
+            for(int j=i+1;j<n;j++){
+                if(arr[j][0]<=end){
+                    end=Math.max(end, arr[j][1]);
+                }
+            }
+            res.add(new int[]{start,end});
         }
+        return res.toArray(new int[res.size()][]);
     }
     public static void main(String[] args) {
-        
+        int arr[][]={{1,3},{2,6},{8,10},{15,18}};
+        int ans[][]=brute(arr);
+        int n=ans.length;
+        for(int i=0;i<n;i++){
+            System.out.print(Arrays.toString(ans[i])+" ");
+        }
+        System.out.println();
     }
 }
