2f4fd114d3a4d66ed9c195f45bb0fff3c45115c3
Suraj Senapati <surajsenapati58@gmail.com>
2025-06-26 20:35:34 +0530

    Another solution for climbing stairs

--- DynamicProgramming/ClimbingStairs.java
+++ DynamicProgramming/ClimbingStairs.java
@@ -12,6 +12,18 @@ public class ClimbingStairs {
         }
         return curr;
     }
+    public int climbStairs(int n) {
+        int arr[]=new int[n+1];
+        if (n<=1) return 1;
+        else{
+            arr[0]=1;
+            arr[1]=1;
+            for(int i=2;i<=n;i++){
+                arr[i]=arr[i-1]+arr[i-2];
+            }
+        }
+        return arr[n];
+    }
     public static void main(String[] args) {
         int n=4;
         System.out.println(count(n));
