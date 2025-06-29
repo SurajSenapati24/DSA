248cd8b21d4760f9bc98ccd741553d8e721be52f
Suraj Senapati <surajsenapati58@gmail.com>
2025-06-29 22:23:49 +0530

    Deletion of a node in linkedList 237

---
+++ LinkedLists/DeleteNodeInALL.java
@@ -0,0 +1,34 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+public class DeleteNodeInALL {
+    public static void deleteNode(ListNode node){
+        node.val=node.next.val;
+        node.next=node.next.next;
+    }
+    public static void print(ListNode head){
+        ListNode temp=head;
+        while(temp.next!=null){
+            System.out.print(temp.val+" ");
+            temp=temp.next;
+        }
+        System.out.print(temp.val+" ");
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        ListNode head=new ListNode(1);
+        ListNode one=new ListNode(2);
+        head.next=one;
+        ListNode two=new ListNode(3);
+        one.next=two;
+        ListNode three=new ListNode(4);
+        two.next=three;
+        ListNode four=new ListNode(5);
+        three.next=four;
+        print(head);
+        deleteNode(three);
+        print(head);
+    }
+}
---
+++ LinkedLists/LinkedList.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        this.data=data;
+        next=null;
+    }
+    public Node(int data, Node next){
+        this.data=data;
+        this.next=next;
+    }
+}
+public class LinkedList {
+    public static void main(String[] args) {
+        
+    }
+}
