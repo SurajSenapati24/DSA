4d68c101202923605a0f6ade3268aaeb981caa8c
Suraj Senapati <surajsenapati58@gmail.com>
2025-07-01 20:09:58 +0530

    update

--- LinkedLists/LinkedListImplementation.java
+++ LinkedLists/LinkedListImplementation.java
@@ -37,12 +37,31 @@ public class LinkedListImplementation {
         temp.next=new Node(val);
     }
     public static void insertAt(Node head, int val, int position){
-
+        Node temp=head;
+    }
+    public static Node ArraytoLL(int arr[]){
+        Node head=new Node(arr[0]);
+        Node temp=head;
+        for(int i=1;i<arr.length;i++){
+            temp.next=new Node(arr[i]);
+            temp=temp.next;
+        }
+        return head;
+    }
+    public static void delete(Node node){
+        node.data=node.next.data;
+        node.next=node.next.next;
     }
     public static void main(String[] args) {
         Node head=new Node(0);
         insert(head, 1);
         insert(head, 2);
+        insert(head, 3);
         traverse(head);
+        delete(head);
+        traverse(head);
+        int []arr={1,2,4,5,6,7};
+        Node newhead=ArraytoLL(arr);
+        traverse(newhead);
     }
 }
