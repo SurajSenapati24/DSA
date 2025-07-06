096fea2e983163551cc1409124ba446d511a986a
Suraj Senapati <surajsenapati58@gmail.com>
2025-07-06 22:44:44 +0530

    LengthofLoop in LL

---
+++ Graphs/FloodFillAlgorithm.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class FloodFillAlgorithm{
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+       int initaial=image[sr][sc];
+       image[sr][sc]=color;
+    }
+    public static void main(String[] args) {
+        int [][] matrix={{1,1,1},{1,1,0},{1,0,1}};
+        int sr = 1, sc = 1, color = 2;
+    }
+}
