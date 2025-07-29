7459df7ca5be79a43abf334612bd61303e1fbc48
Suraj Senapati <surajsenapati58@gmail.com>
2025-07-29 22:34:22 +0530

    update

---
+++ LinkedLists/DoublyLL/DoublyLinkedList.java
@@ -0,0 +1,19 @@
+package DoublyLL;
+class Node{
+    int data;
+    Node prev;
+    Node next;
+    public Node(int data){
+        this.data=data;
+        prev=null;
+        next=null;
+    }
+    public Node(int data, Node prev, Node next){
+        this.data=data;
+        this.prev=prev;
+        this.next=next;
+    }
+}
+public class DoublyLinkedList {
+    
+}
