bc0c755615665c9e4d9a97cd4fefa05546b558a7
Suraj Senapati <surajsenapati58@gmail.com>
2025-08-01 22:54:11 +0530

    80 Remove Elements From An array 2

--- Leetcode150/RemoveElementsFromAnArray2.java
+++ Leetcode150/RemoveElementsFromAnArray2.java
@@ -1,3 +1,19 @@
 public class RemoveElementsFromAnArray2 {
-    
+    public int removeDuplicates(int[] nums) {
+        int l=1;
+        int count=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
+                count++;
+            }
+            else{
+                count=1;
+            }
+            if(count<=2){
+                nums[l]=nums[i];
+                l++;
+            }
+        }
+        return l;
+    }
 }
