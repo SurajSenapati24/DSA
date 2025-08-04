a8acd3f1b7e281118a24e7f44385290858049195
Suraj Senapati <surajsenapati58@gmail.com>
2025-08-04 22:58:42 +0530

    55 Jump Game

---
+++ Leetcode150/JumpGame.java
@@ -0,0 +1,10 @@
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int maxx=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>maxx) return false;
+            maxx=Math.max(maxx, i+nums[i]);
+        }
+        return true;
+    }
+}
