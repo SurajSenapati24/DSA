3373a3f39890d3f93b029f5e526de4f2bacb844a
Suraj Senapati <surajsenapati58@gmail.com>
2025-08-20 22:35:05 +0530

    Sort An array with 0s 1s and 2s

---
+++ Final450/Array/Sort0s1s2s.java
@@ -0,0 +1,22 @@
+public class Sort0s1s2s {
+    public static void swap(int arr[], int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+    public static void soltuion(int arr[]){
+        int low=0,mid=0,high=arr.length-1;
+        while(mid <= high) {
+            if(arr[mid] == 0) {
+                swap(arr, low, mid);
+                low++;
+                mid++;
+            } else if(arr[mid] == 1) {
+                mid++;
+            } else {
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+    }
+}
