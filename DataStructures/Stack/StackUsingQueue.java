13e12eba086dd6661bd5887f4d8b3ea8c8dd9a00
Suraj Senapati <surajsenapati58@gmail.com>
2025-09-04 10:54:48 +0530

    Stack Using Queue

--- DataStructures/Stack/StackUsingQueue.java
+++ DataStructures/Stack/StackUsingQueue.java
@@ -1,3 +1,42 @@
+import java.util.LinkedList;
+import java.util.Queue;
 public class StackUsingQueue {
-    
+    static Queue<Integer> q1=new LinkedList<>();
+    static Queue<Integer> q2=new LinkedList<>();
+    public static void push(int val){
+        while(q1.size()!=0){
+            q2.offer(q1.poll());
+        }
+        q1.offer(val);
+        while(q2.size()!=0){
+            q1.offer(q2.poll());
+        }
+    }
+    public static void pop(){
+        if(q1.size()==0){
+            System.out.println("Queue is empty");
+        }
+        else{
+            System.out.println(q1.poll());
+        }
+    }
+    public static void peek(){
+        if(q1.size()==0){
+            System.out.println("Queue is empty");
+        }
+        else{
+            System.out.println(q1.peek());
+        }
+    }
+    public static void size(){
+        System.out.println(q1.size());
+    }
+    public static void main(String[] args) {
+        push(1);
+        push(2);
+        push(3);
+        peek();
+        pop();
+        size();
+    }
 }
