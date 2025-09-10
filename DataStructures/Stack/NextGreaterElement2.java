dfc651d9575e2eddbf52f9b2ca84da9d766e37ba
Suraj Senapati <surajsenapati58@gmail.com>
2025-09-10 22:38:23 +0530

    Next Greater Element 2

---
+++ DataStructures/Stack/NextGreaterElement2.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Stack;
+public class NextGreaterElement2 {
+    public static int[] optimal(int nums[]){
+        int n=nums.length;
+        int ans[]=new int[n];
+        Stack<Integer> st=new Stack<>();
+        for(int i=2*n-1;i>=0;i--){
+            int curr=nums[i%n];
+            while(!st.isEmpty() && st.peek()<=curr){
+                st.pop();
+            }
+            if(i<n){
+                ans[i%n]=(st.isEmpty() ? -1 : st.peek());
+            }
+            st.push(nums[i%n]);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,1};
+        System.out.println(Arrays.toString(optimal(arr)));
+    }
+}
